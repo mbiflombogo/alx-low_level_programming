a9b5a00d31cb1b638b72b6f83350d3413ee75355
Florence Mbogo <karimiflorence7579@gmail.com>
2022-07-04 03:46:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * master - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int master(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
