f1f8e936642112fc1cb2bfa5bb86ea702f833629
Florence Mbogo <karimiflorence7579@gmail.com>
2022-07-06 10:50:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0
+
+		for (i = 0; i < 8; i++)
+		{
+			putchar(text[i]);
+		}
+putchar('\n');
+
+return (0);
+}
