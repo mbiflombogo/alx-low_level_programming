5406a7c649a969f2a72680e6f6cb610ab91adb3f
Florence Mbogo <karimiflorence7579@gmail.com>
2022-07-06 17:04:58 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,12 +17,12 @@ void times_table(void)
 		for (column = 0; column <= 9; column++)
 		{
 			product = row * column;
-			tens product / 10;
+			tens = product / 10;
 			ones = product % 10;
 
 			if (column == 0)
 			{
-				-putchar('0');
+				_putchar('0');
 			}
 			else if (product < 10)
 			{
