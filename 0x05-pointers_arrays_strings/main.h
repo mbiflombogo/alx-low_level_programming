30e0f72933dc9bb1444451537b5bbb6e57e4e889
Florence Mbogo <karimiflorence7579@gmail.com>
2022-07-11 14:41:09 -0700

    putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,4 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
-void _putchar (char c); 
+void _putchar (char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,5 +9,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif 
+int _atoi(char *s);
