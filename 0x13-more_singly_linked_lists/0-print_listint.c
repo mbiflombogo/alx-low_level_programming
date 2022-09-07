4089aaaf6e6b638f8523e950b2148879874d79ab
Florence Mbogo <karimiflorence7579@gmail.com>
2022-09-07 08:47:26 -0700

    0x13

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_listint - prints all the elements of a list.
  * @h: head of a list.
@@ -8,13 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t nnodes = 0;
+size_t count = 0;
+const listint_t *copy = h;
 
-while (h != NULL)
+while (copy != NULL)
 {
 printf("%d\n", h->n);
-h = h->next;
-nnodes++;
+copy = copy->next;
+count++;
 }
-return (nnodes);
+return (count);
 }
